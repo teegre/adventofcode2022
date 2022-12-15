c885972fd5348384b1c96b7e5b22f3fbf2826e2a
Stéphane MEYER <stm3y3r@icloud.com>
2022-12-15 15:32:14 +0100

    04-2

--- 04/solution.py
+++ 04/solution.py
@@ -2,8 +2,10 @@
 
 # Part one
 acc = 0
+acc2 = 0
 inp = []
 inrange = lambda a,b,c,d: set(range(a,b+1)).issubset(range(c,d+1)) or set(range(c,d+1)).issubset(range(a,b+1))
+intersect = lambda a,b,c,d: len(set(range(a,b+1)).intersection(set(range(c,d+1)))) > 0
 with open('input.txt', 'r') as f:
   for line in f.readlines():
     line = line[:-1]
@@ -13,4 +15,7 @@ with open('input.txt', 'r') as f:
     r = [int(r11),int(r12),int(r21),int(r22)]
     if inrange(*r):
       acc += 1
+    if intersect(*r):
+      acc2 += 1
 print('part one:', acc)
+print('part two:', acc2)
