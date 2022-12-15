e119a5f858f6ce64ff9911bdf12da07f7fa8225d
Stéphane MEYER <stm3y3r@icloud.com>
2022-12-15 11:52:38 +0100

    03-2

--- 03/solution.py
+++ 03/solution.py
@@ -1,6 +1,7 @@
 """ Rucksack Reorganization """
-
 from itertools import chain
+
+# Part one
 abc = [chr(c) for c in chain(range(97,123),range(65,91))]
 acc = 0
 
@@ -11,3 +12,21 @@ with open('input.txt', 'r') as f:
     item = { it for it in chain(p1,p2) if it in p1 and it in p2 }.pop()
     acc += abc.index(item)+1
 print('part one:', acc)
+
+# Part two
+groups = []
+group = []
+acc = 0
+with open('input.txt', 'r') as f:
+  for i,line in enumerate(f.readlines(), 1):
+    line = line[:-1]
+    group.append(line)
+    if i%3 == 0:
+      groups.append(group)
+      group = []
+
+for group in groups:
+  p1, p2, p3 = set(group[0]), set(group[1]), set(group[2])
+  item = { it for it in chain(p1,p2,p3) if it in p1 and it in p2 and it in p3 }.pop()
+  acc += abc.index(item)+1
+print('part two:', acc)
