bc20c6dc593dc977404148784003683623cbc017
Stéphane MEYER <stm3y3r@icloud.com>
2022-12-16 09:31:42 +0100

    06-2

--- 06/solution.py
+++ 06/solution.py
@@ -11,3 +11,10 @@ for i in range(len(inp)):
   if len(set(inp[i:i+4])) == 4:
     print('part one:', i+4)
     break
+
+# Part two
+start = i+5
+for i in range(start,len(inp)):
+  if len(set(inp[i:i+14])) == 14:
+    print('part two:', i+14)
+    break
