f1ba033d9495e884fdfdf2c54e941e2566370930
Stéphane MEYER <tigerlost@me.com>
2022-12-17 10:02:49 +0100

    tree+file objects

--- 07/solution.py
+++ 07/solution.py
@@ -10,44 +10,52 @@ with open('input.txt', 'r') as f:
 test = ['$ cd /','$ ls', 'dir a', '14848514 b.txt', '8504156 c.dat', 'dir d', '$ cd a', '$ ls', 'dir e', '29116 f', '2557 g', '62596 h.lst', '$ cd e', '$ ls', '584 i', '$ cd ..', '$ cd ..', '$ cd d', '$ ls', '4060174 j', '8033020 d.log', '5626152 d.ext', '7214296 k']
 
 class Tree:
-  def __init__(self, root):
-    self.root = root
+  def __init__(self, dir_name):
+    self.parent = None
+    self.dir = dir_name
     self.children = []
-    self.nodes = []
-  def add(self, obj):
-    self.children.append(obj)
-  def get_all_nodes(self):
-    self.nodes.append(self.root)
+  def add(self, *objs):
+    for obj in objs:
+      if isinstance(obj, (Tree, File)):
+        obj.parent = self
+      self.children.append(obj)
+  def get(self, name):
     for child in self.children:
-      self.nodes.append(child.data)
-      if child.get_child_nodes(self.nodes):
-        child.get_child_nodes(self.nodes)
+      if isinstance(child, Tree):
+        if child.dir == name:
+          return child
+    return None
   def __str__(self):
-    return self.root
+    return self.dir
   def __repr__(self):
-    return f'tree: {self.root} → {self.children}'
+    return f'tree: {self.dir} → {self.children}'
 
-class Node:
-  def __init__(self, data):
-    self.data = data
-    self.children = []
-  def add(self, obj):
-    self.children.append(obj)
-  def get_child_nodes(self, tree):
-    for child in self.children:
-      if child.children(tree):
-        tree.append(child.data)
-      else:
-        tree.append(child.data)
+class File:
+  def __init__(self, name, size):
+    self.parent = None
+    self.name = name
+    self.size = size
   def __str__(self):
-    return self.data
+    return self.name
   def __repr__(self):
-    return f'node: {self.data} → {self.children}'
+    return f'file: {self.name} size={self.size}'
 
-tree = Tree('/')
+root = Tree('/')
 
+test.pop(0)
+current = root
 for line in test:
-  if line.startswith('$ cd'):
-    _,_,dir = line.split()
-    tree[dir] = {'.': []}
+  if line.startswith('$'): # command
+    l = line.split()
+    if len(l) == 3:
+      _,_,d = l
+      if d == '..':
+        current = current.parent
+      else:
+        current = current.get(d)
+  elif line.startswith('dir'):
+    current.add(Tree(line.split()[1]))
+  else:
+    s,f = line.split()
+    current.add(File(f,int(s)))
 
