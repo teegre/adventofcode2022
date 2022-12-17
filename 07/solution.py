8f0c40240548d6f11e2d64dff7ea591c483f3819
Stéphane MEYER <tigerlost@me.com>
2022-12-17 19:51:12 +0100

    07-2

--- 07/solution.py
+++ 07/solution.py
@@ -1,50 +1,55 @@
 """ Day 07 : No Space Left On Device """
 
-# Part one
-
 inp = []
 with open('input.txt', 'r') as f:
   for line in f.readlines():
     inp.append(line[:-1])
 
-test = ['$ cd /','$ ls', 'dir a', '14848514 b.txt', '8504156 c.dat', 'dir d', '$ cd a', '$ ls', 'dir e', '29116 f', '2557 g', '62596 h.lst', '$ cd e', '$ ls', '584 i', '$ cd ..', '$ cd ..', '$ cd d', '$ ls', '4060174 j', '8033020 d.log', '5626152 d.ext', '7214296 k']
-
-class Tree:
+class Directory:
   def __init__(self, dir_name):
     self.parent = None
     self.dir = dir_name
     self.children = []
   def add(self, *objs):
     for obj in objs:
-      if isinstance(obj, (Tree, File)):
+      if isinstance(obj, (Directory, File)):
         obj.parent = self
       self.children.append(obj)
   def get(self, name):
     for child in self.children:
-      if isinstance(child, Tree):
+      if isinstance(child, Directory):
         if child.dir == name:
           return child
     return None
+  @property
+  def size(self):
+    return sum(f.size for f in self.children)
+  def __iter__(self):
+    for item in self.children:
+      yield item
+      if isinstance(item, Directory):
+        for subitem in item:
+          yield subitem
   def __str__(self):
     return self.dir
   def __repr__(self):
-    return f'tree: {self.dir} → {self.children}'
+    return f'dir: {self.dir} (size={self.size})→ {self.children}'
 
 class File:
   def __init__(self, name, size):
     self.parent = None
     self.name = name
-    self.size = size
+    self.size = int(size)
   def __str__(self):
     return self.name
   def __repr__(self):
     return f'file: {self.name} size={self.size}'
 
-root = Tree('/')
+root = Directory('/')
 
-test.pop(0)
+inp.pop(0)
 current = root
-for line in test:
+for line in inp:
   if line.startswith('$'): # command
     l = line.split()
     if len(l) == 3:
@@ -54,8 +59,15 @@ for line in test:
       else:
         current = current.get(d)
   elif line.startswith('dir'):
-    current.add(Tree(line.split()[1]))
+    current.add(Directory(line.split()[1]))
   else:
     s,f = line.split()
     current.add(File(f,int(s)))
 
+# Part one
+total = sum(item.size if item.size <= 100000 else 0 for item in root if isinstance(item, Directory))
+print('part one:', total)
+
+# Part two
+sizes = [d.size for d in root if isinstance(d, Directory) and d.size >= 30000000-(70000000-root.size)]
+print('part two:', min(sizes))
