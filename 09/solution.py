547a9d8041948eaf2559f11947afed2e6a523697
Stéphane MEYER <tigerlost@me.com>
2022-12-20 07:17:26 +0100

    09-1 update

--- 09/solution.py
+++ 09/solution.py
@@ -1,1 +1,25 @@
+""" Day 09 - Rope Bridge """
 
+with open('input.txt', 'r', encoding='utf-8') as f:
+  inp = [ l[:-1] for l in f.readlines() ]
+
+test = [ 'R 4', 'U 4', 'L 3', 'D 1', 'R 4', 'D 1', 'L 5', 'R 2' ]
+
+class Knots:
+  def __init__(self, heady=0, headx=0, taily=0, tailx=0):
+    self.hy = heady
+    self.hx = headx
+    self.ty = taily
+    self.tx = tailx
+  def move_up(self):
+    hy = self.hy
+    hx = self.hx
+    self.hy += 1
+
+  @property
+  def pos(self):
+    return self.hy, self.hx, self.ty, self.tx
+  def __str__(self):
+    return f'{self.pos}'
+  def __repr__(self):
+    return f'knots: {self.pos}'
