80451ca116d5eaf11d586450a2c34b2d7ec42cdd
Stéphane MEYER <tigerlost@me.com>
2022-12-21 20:06:56 +0100

    09-1 almost

--- 09/solution.py
+++ 09/solution.py
@@ -16,31 +16,39 @@ class Knots:
   def move_up(self, dist=1):
     """ Move head up and record tail positions. """
     self.hy += dist
-    if self.tx != self.hx:
-      for x in range(self.tx, self.hx, 1 if self.tx < self.hx else -1):
-        self.tpos.add((self.ty, x))
-      self.tx = self.hx
+    for y in range(self.ty, self.ty+dist, 1 if self.ty < self.ty+dist else -1):
+      if y == self.ty+1:
+        self.tx = self.hx
+      if self.tx == self.hx:
+        self.tpos.add((y, self.hx))
+    self.ty += dist-1
   def move_down(self, dist=1):
     """ Move head down and record tail positions. """
     self.hy -= dist
-    if self.tx != self.hx:
-      for x in range(self.tx, self.hx, 1 if self.tx < self.hx else -1):
-        self.tpos.add((self.ty, x))
-      self.tx = self.hx
+    for y in range(self.ty, self.ty-dist, 1 if self.ty < self.ty-dist else -1):
+      if y == self.ty-1:
+        self.tx = self.hx
+      if self.tx == self.hx:
+        self.tpos.add((y, self.hx))
+    self.ty -= dist-1
   def move_right(self, dist=1):
     """ Move head right and record tail positions. """
     self.hx += dist
-    if self.ty != self.hy:
-      for y in range(self.ty, self.hy, 1 if self.ty < self.hy else -1):
-        self.tpos.add((y, self.tx))
-      self.ty = self.hy
+    for x in range(self.tx, self.tx+dist, 1 if self.tx < self.hx+dist else -1):
+      if x == self.tx+1:
+        self.ty = self.hy
+      if self.ty == self.hy:
+        self.tpos.add((self.ty, x))
+    self.tx += dist-1
   def move_left(self, dist=1):
     """ Move head left and record tail positions """
     self.hx -= dist
-    if self.ty != self.hy:
-      for y in range(self.ty, self.hy, 1 if self.ty < self.hy else -1):
-        self.tpos.add((y, self.tx))
-      self.ty = self.hy
+    for x in range(self.tx, self.tx-dist, 1 if self.tx < self.hx-dist else -1):
+      if x == self.tx-1:
+        self.ty = self.hy
+      if self.ty == self.hy:
+        self.tpos.add((self.ty, x))
+    self.tx -= dist-1
   @property
   def pos(self):
     """ Return current position. """
@@ -54,7 +62,7 @@ class Knots:
   def __str__(self):
     return f'head: y={self.hy} x={self.hx} | tail: y={self.ty} x={self.tx}'
   def __repr__(self):
-    return f'knots: [head:({self.hy},{self.hx})|tail:({self.ty},{self.ty})]'
+    return f'knots: [head:({self.hy},{self.hx})|tail:({self.ty},{self.tx})]'
 
 knots = Knots()
 
